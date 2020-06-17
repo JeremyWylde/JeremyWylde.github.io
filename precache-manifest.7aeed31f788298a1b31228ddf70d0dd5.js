c827d2deb84038240d3a11148eed2a5a0012862d
JeremyWylde <63048608+JeremyWylde@users.noreply.github.com>
2020-06-17 07:29:06 +0300

    Updates

---
+++ precache-manifest.7aeed31f788298a1b31228ddf70d0dd5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcb9b2f5a3c8fa31eb2b932d02043332",
+    "url": "//jeremywylde.github.io/portfolio/index.html"
+  },
+  {
+    "revision": "8d67b48a724c0fa2bbf0",
+    "url": "//jeremywylde.github.io/portfolio/static/css/main.01f3b4ba.chunk.css"
+  },
+  {
+    "revision": "e6b79daa193820b5625c",
+    "url": "//jeremywylde.github.io/portfolio/static/js/2.766c329b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//jeremywylde.github.io/portfolio/static/js/2.766c329b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d67b48a724c0fa2bbf0",
+    "url": "//jeremywylde.github.io/portfolio/static/js/main.66200927.chunk.js"
+  },
+  {
+    "revision": "3cd95bbbca5df53c6006",
+    "url": "//jeremywylde.github.io/portfolio/static/js/runtime-main.a16dff3f.js"
+  },
+  {
+    "revision": "e0b499ef6c6dfb10735be3fa053df133",
+    "url": "//jeremywylde.github.io/portfolio/static/media/HTML&CSS.e0b499ef.svg"
+  },
+  {
+    "revision": "d2f6f0a1e5c6fc191ed1cedf8c5dee7b",
+    "url": "//jeremywylde.github.io/portfolio/static/media/JS.d2f6f0a1.svg"
+  },
+  {
+    "revision": "22e179e9e0dbb92c5f2e2ed5b6ca737c",
+    "url": "//jeremywylde.github.io/portfolio/static/media/React.22e179e9.svg"
+  },
+  {
+    "revision": "e9ed364132d1701a41b0f768630d80f2",
+    "url": "//jeremywylde.github.io/portfolio/static/media/Redux.e9ed3641.svg"
+  },
+  {
+    "revision": "aa2085b12bcfeb989d433a2de2c32ea8",
+    "url": "//jeremywylde.github.io/portfolio/static/media/SN.aa2085b1.jpg"
+  },
+  {
+    "revision": "ad14b501caa69cc720080517539435a4",
+    "url": "//jeremywylde.github.io/portfolio/static/media/github.ad14b501.svg"
+  },
+  {
+    "revision": "4783131d2b0ba27d29ddf185a23b0c73",
+    "url": "//jeremywylde.github.io/portfolio/static/media/telegram.4783131d.svg"
+  },
+  {
+    "revision": "20b26e52a8f62402f404860c98779ae3",
+    "url": "//jeremywylde.github.io/portfolio/static/media/toDolist.20b26e52.jpg"
+  },
+  {
+    "revision": "258a9e69a7fed9f26c79a912d3c113c1",
+    "url": "//jeremywylde.github.io/portfolio/static/media/vk-1.258a9e69.svg"
+  }
+]);
