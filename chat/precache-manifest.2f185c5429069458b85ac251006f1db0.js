221025c19fbeced5e1ad89d815a217a25721cc3c
JeremyWylde <63048608+JeremyWylde@users.noreply.github.com>
2020-06-26 21:29:53 +0300

    added chat

---
+++ chat/precache-manifest.2f185c5429069458b85ac251006f1db0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a44583eb00d239ec9cf883692f4f29a5",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "34cb508322b16aae2eb0",
+    "url": "/chat/static/css/main.cf963a2d.chunk.css"
+  },
+  {
+    "revision": "a64bd22bb09f2a9cacea",
+    "url": "/chat/static/js/2.8e655311.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/chat/static/js/2.8e655311.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34cb508322b16aae2eb0",
+    "url": "/chat/static/js/main.c1da994d.chunk.js"
+  },
+  {
+    "revision": "8d86be0bcc4812848edd",
+    "url": "/chat/static/js/runtime-main.6b736942.js"
+  }
+]);
